6558dbbc7e63baaba298e3d0ec2cb4c806642eed
Tam Dao <tamminhdaoabbas@gmail.com>
2017-03-07 10:31:29 -0600

    final

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -27,32 +27,32 @@ $(function() {
         });
 
 
-        /* TODO: Write a test that loops through each feed
+        /* Write a test that loops through each feed
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
         it('all feeds have valid URLs', function () {
-            allFeeds.forEach (function (item) {
-                expect (item.url).toBeDefined();
-                expect (item.url).not.toBe("");
-            })
+            allFeeds.forEach (function (feed) {
+                expect (feed.url).toBeDefined();
+                expect (feed.url).not.toBe("");
+            });
         });
 
 
-        /* TODO: Write a test that loops through each feed
+        /* Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
         it('all feeds have valid names', function () {
-            allFeeds.forEach (function (item) {
-                expect (item.name).toBeDefined();
-                expect (item.name).not.toBe("");
-            })
+            allFeeds.forEach (function (feed) {
+                expect (feed.name).toBeDefined();
+                expect (feed.name).not.toBe("");
+            });
         });
     });
 
 
-    /* TODO: Write a new test suite named "The menu" */
+    /* Write a new test suite named "The menu" */
     describe ('The menu', function () {
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
@@ -63,7 +63,7 @@ $(function() {
             expect ($('body').hasClass('menu-hidden')).toBeTruthy();
          });
 
-         /* TODO: Write a test that ensures the menu changes
+         /* Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
@@ -78,7 +78,7 @@ $(function() {
     });
 
 
-    /* TODO: Write a new test suite named "Initial Entries" */
+    /* Write a new test suite named "Initial Entries" */
     describe ('Initial Entries', function() {
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
@@ -88,25 +88,22 @@ $(function() {
          */
         beforeEach (function(done) {
             //Load Udacity Feed
-            loadFeed (0, function () {
-                done();
-            });
+            loadFeed (0, done);
         });
 
-        it('should show at least one entry in the feed container', function(done) {
-            var container = $('.feed');
-            expect (container.length).toBeGreaterThan(0);
-            done();
+        it('should show at least one entry in the feed container', function() {
+            var feedEntry = $('.feed .entry');
+            expect (feedEntry.length).toBeGreaterThan(0);
         });
     });
 
 
-    /* TODO: Write a new test suite named "New Feed Selection" */
+    /* Write a new test suite named "New Feed Selection" */
     describe ('New Feed Selection', function() {
         var contentCSS,
             contentHTML; 
 
-        /* TODO: Write a test that ensures when a new feed is loaded
+        /* Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          * Call loadFeeds twice and safe each feed's content in a variable. Compare 2 variables.
@@ -123,9 +120,8 @@ $(function() {
             });
         });
 
-        it('should change feeds content', function(done) {
+        it('should change feeds content', function() {
             expect (contentCSS).not.toEqual(contentHTML);
-            done();
-        })
+        });
     });
 }());
